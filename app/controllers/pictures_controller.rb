be0d5ab3984cfa534c35f483d76bda95d266c7a7
Matt Cordoba <cordobamatt@gmail.cp>
2013-05-12 15:17:43 -0700

    Updated app and public

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,6 @@
 class PicturesController < ApplicationController
   def index
     @artist = Artist.find(params[:artist_id])
-    @song = Song.find(params[:song_id])
-    @pictures = @song.pictures.all
+    
   end
 end
