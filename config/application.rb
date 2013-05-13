d871e8c202f6f7906102def8a7d90b5b8089eb58
Matt Cordoba <cordobamatt@gmail.cp>
2013-05-13 11:19:04 -0700

    more config changes

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,16 @@ module App
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
-    config.assets.compile = false
+    config.assets.precompile += ['mobile.css', 'mobile.js']
+     # Not required, but a good best-practice for performance.
+        # This setting will compress your assets as much as possible using YUI and Uglifier by default
+    config.assets.compress = true
+        
+        # Allow fingerprinting of asset filenames - good for caching.
+    config.assets.digest = true
+        
+        # Configure the sendfile headers for Heroku.  "X-Accel-Redirect" is also a good value for this since Heroku use Nginx.
+    config.action_dispatch.x_sendfile_header = nil
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
